dfaaa40a1ccbf8ba716774e9af1a0cd972e65297
Administrator <Administrator@4654-PC>
2019-04-24 10:17:03 +0800

    Signed-off-by: Administrator <Administrator@4654-PC> format

--- java8/src/main/java/com/dj/bean/User.java
+++ java8/src/main/java/com/dj/bean/User.java
@@ -9,17 +9,21 @@ public class User {
 	 * 年龄
 	 */
 	private int age;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
+
 }
