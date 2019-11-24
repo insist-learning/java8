505c2ebcccf8e162323e0491af32e62a83f25bbc
thismygithubcom <40890438+thismygithubcom@users.noreply.github.com>
2019-11-25 00:14:29 +0800

    Update Problem.java

--- java8/src/main/java/com/dj/leetCode/Problem.java
+++ java8/src/main/java/com/dj/leetCode/Problem.java
@@ -3,7 +3,7 @@ package com.dj.leetCode;
 public class Problem {
   //一点注释，再加一点注释，。。。。，我也加一点注释
 	public static void main(String[] args) {
-		System.out.println("This is a problem");
+		System.out.println("This is a problem fetch");
 	}
 }
 
