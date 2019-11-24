db7112ad3b9825c7b2fdf3c6dbd748bc7405ae29
ldj <11@.com>
2019-11-24 23:16:08 +0800

    11

---
+++ java8/src/main/java/com/dj/main/TestThread.java
@@ -0,0 +1,55 @@
+package com.dj.main;
+
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TestThread {
+
+
+	public static void main(String[] args) throws Exception{
+		ConcurrentHashMap<String, Boolean> map = new ConcurrentHashMap<String, Boolean>(1);
+
+		// 吊转小车
+		Thread thread2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				synchronized (this) {
+					try {
+						Thread.currentThread().wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				System.out.println("执行线程2");
+
+			}
+		});
+		// 变轨小车
+		Thread thread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("执行线程1");
+				Thread.currentThread().notify();
+//				while (true) {
+//					System.out.println("执行线程1");
+//					map.put("action", true);
+//					try {
+//						Thread.currentThread().sleep(650);
+//					} catch (InterruptedException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//				}
+			}
+
+		});
+
+		thread.start();
+		thread2.start();
+
+	}
+
+}
